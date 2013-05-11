d4886e518ae629bbc715a5569f055432f27884fe
Justin <moultinjust@gmail.com>
2013-05-11 11:07:25 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Moultonjr::Application.config.secret_token = 'cf08ad000edc29d0e237707f024525d7ae5dd4d230f1869c236c26519b9f2bcb7bbb1613d2da815ed22edddc5bd7a5ef4ef44c5fadfd9eda0e520aa9d32bee5e'
